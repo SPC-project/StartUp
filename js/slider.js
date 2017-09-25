790072fad168a5a76388f750a12882848dc480ce
margarita <margaritasklyarenko@gmail.com>
2017-09-26 02:18:59 +0300

    smth happened with index.php

---
+++ js/slider.js
@@ -0,0 +1,20 @@
+var current = 1,
+	imgs = document.getElementById("particles-js").getElementsByTagName("IMG");
+
+setTimeout(function tick() {
+	if (current == imgs.length) {
+		current = 0;
+	}
+	/*document.getElementById("particles-js").classList.toggle("disdlay_none");
+	 */
+	for (var l = 0; l < imgs.length; l++) {
+		if (l == current)
+			imgs[l].classList.remove("display_none");
+		else imgs[l].classList.add("display_none");
+		console.log(imgs[l], imgs[l].classList);
+	}
+
+
+	current++;
+	timerId = setTimeout(tick, 4000);
+}, 4000);
