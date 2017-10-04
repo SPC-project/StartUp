c61c3e5c1750cbbbccbb4bd93d223891e516559c
Ivan Zadorozhniy <vakikrin@gmail.com>
2017-10-04 19:53:00 +0300

    Update 2.0 for confirmation by email, added hash function for link

--- conf.php
+++ conf.php
@@ -1,9 +1,9 @@
 <?php
     require "db.php"; //Подключаемся к БД
 	$errors = array(); //Массив ошибок
-		$user = R::findOne('users','login = ?',array($_GET['login']));
+		$user = R::findOne('users','for_verification = ?',array($_GET['login']));
 if( isset($user)) echo 'w'; else echo 'r';
-echo $myHash;
+
 	if($user->is_verification == 0) { $user->is_verification = 1; R::store($user);}
 header('Location: index.php');
 ?>
